3391b490f4cfbad3c91bdf8d31fb21de6aa068e7
tomas-holub <tomas-holub@users.noreply.github.com>
2015-02-09 01:08:53 +0100

    Version 0.1 created :)

--- coal.js
+++ coal.js
@@ -1,127 +1,135 @@
-    Scope.prototype.$apply = function(expr) {
-        try {
-            return this.$eval(expr);
-        } finally {
-            this.$digest();
+var coal = (function () {
+
+        function Scope() {
+            this.$$watchers = [];
+            this.$$asyncQueue = [];
         }
-    };
 
-    Scope.prototype.$eval = function(expr, locals) {
-        return expr(this, locals);
-    };
-
-    Scope.prototype.$evalAsync = function(expr) {
-        this.$$asyncQueue.push({scope: this, expression: expr});
-    };
-
-    Scope.prototype.$watch = function(watchFn, listenerFn) {
-        var watcher = {
-            watchFn: watchFn,
-            listenerFn: listenerFn || function() { }
+        Scope.prototype.$apply = function (expr) {
+            try {
+                return this.$eval(expr);
+            } finally {
+                this.$digest();
+            }
         };
-        this.$$watchers.push(watcher);
-    };
 
-    Scope.prototype.$$digestOnce = function() {
-        var self = this;
-        var dirty;
-        for(var i=0;i<this.$$watchers.length; i++) {
-            var watch = this.$$watchers[i];
+        Scope.prototype.$eval = function (expr, locals) {
+            return expr(this, locals);
+        };
 
-            var newValue = watch.watchFn(self);
-            var oldValue = watch.last;
+        Scope.prototype.$evalAsync = function (expr) {
+            this.$$asyncQueue.push({scope: this, expression: expr});
+        };
 
-            if (newValue !== oldValue) {
-              watch.listenerFn(newValue, oldValue, self);
-              dirty = true;
-              watch.last = newValue;
+        Scope.prototype.$watch = function (watchFn, listenerFn) {
+            var watcher = {
+                watchFn: watchFn,
+                listenerFn: listenerFn || function () {
+                }
+            };
+            this.$$watchers.push(watcher);
+        };
+
+        Scope.prototype.$$digestOnce = function () {
+            var self = this;
+            var dirty;
+            for (var i = 0; i < this.$$watchers.length; i++) {
+                var watch = this.$$watchers[i];
+
+                var newValue = watch.watchFn(self);
+                var oldValue = watch.last;
+
+                if (newValue !== oldValue) {
+                    watch.listenerFn(newValue, oldValue, self);
+                    dirty = true;
+                    watch.last = newValue;
+                }
             }
-        }
-        return dirty;
-    };
+            return dirty;
+        };
 
-    Scope.prototype.$digest = function() {
-        var ttl = 10;
-        var dirty;
-        do {
-            while (this.$$asyncQueue.length) {
-                var asyncTask = this.$$asyncQueue.shift();
-                this.$eval(asyncTask.expression);
-            }
-            dirty = this.$$digestOnce();
-            if (dirty && !(ttl--)) {
-                throw "Coal Error: 10 digest iterations reached";
-            }
-        } while (dirty);
-    }
-
-    var scope  = new Scope();
-    var elems  = document.getElementsByTagName('*');
-    var events = {
-        INPUT:'keyup',
-        SELECT:'change'
-    }
-
-    for (var i=0;i<elems.length;i++) {
-        if (elems[i].getAttribute("data-model")!=null) {
-            (function(el) {
-                var model    = el.getAttribute("data-model");
-                scope[model] = el.value;
-                scope.$watch(
-                    function(scope) {
-                        return scope[model];
-                    },
-                    function(newValue, oldValue, scope) {
-                        console.log('newValue ' + newValue);
-                        Controller(scope);
-                    }
-                );
-
-                var event = events[el.tagName];
-
-                el.addEventListener(event, function(e){
-                    scope[model] = e.target.value
-                    scope.$digest();
-                });
-            }(elems[i]));
-        }
-    }
-
-
-    var validator = (function() {
-        var isSet = function (val) {
-            if (typeof val === 'undefined' || val === null || val === '') {
-                return false;
-            }
-            return true;
+        Scope.prototype.$digest = function () {
+            var ttl = 10;
+            var dirty;
+            do {
+                while (this.$$asyncQueue.length) {
+                    var asyncTask = this.$$asyncQueue.shift();
+                    this.$eval(asyncTask.expression);
+                }
+                dirty = this.$$digestOnce();
+                if (dirty && !(ttl--)) {
+                    throw "Coal Error: 10 digest iterations reached";
+                }
+            } while (dirty);
         }
 
-        var isNumeric = function(val) {
-            return typeof(val) != "boolean" && !isNaN(val);
+        function App(element, scope) {
+            this.scope = scope;
+            this.element = element;
+
         }
 
-        var isString = function(val) {
-            var matches = val.match(/\d+/g);
-            if (matches == null) {
-                return true;
+        App.prototype.controller = function (handler) {
+            this.handler = handler || function () {
+            };
+            this.$$compile(this.element, this.scope);
+        }
+
+        App.prototype.$$compile = function (element, scope) {
+
+            if (typeof element === 'undefined') {
+                var elems = document.getElementsByTagName('*');
+            } else {
+                var elems = document.querySelectorAll(element + ' *');
             }
-            return false;
+            var events = {
+                INPUT: 'keyup',
+                SELECT: 'change'
+            }
+            var values = {};
+            for (var i = 0; i < elems.length; i++) {
+                if (elems[i].getAttribute("data-co-value") != null) {
+                    var name = elems[i].getAttribute("data-co-value");
+                    values[name] = elems[i];
+                }
+                if (elems[i].getAttribute("data-co-model") != null) {
+                    (function (el, handler) {
+                        var model = el.getAttribute("data-co-model");
+                        scope[model] = el.value;
+                        scope.$watch(
+                                function (scope) {
+                                    return scope[model];
+                                },
+                                function (newValue, oldValue, scope) {
+                                    if (typeof values[model] !== 'undefined') {
+                                        values[model].innerHTML = newValue;
+                                    }
+                                    handler(scope, model);
+                                }
+                        );
+
+                        var event = events[el.tagName];
+
+                        el['on' + event] = function () {
+                            scope[model] = this.value;
+                            scope.$digest();
+                        }
+
+                    }(elems[i], this.handler));
+                }
+            }
         }
 
-        var isEmail = function(val) {
-            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(val);
+
+        function init(element) {
+            if (typeof element === 'undefined') {
+                element = document;
+            }
+
+            return new App(element, new Scope());
         }
 
         return {
-            isSet:isSet,
-            isNumeric:isNumeric,
-            isString:isString,
-            isEmail:isEmail
+            init: init
         }
     }());
-
-    scope.$digest();
-    function Controller($scope) {
-        // working with variables on scope
-    }
